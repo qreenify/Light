1d389302704a90859113426ab4af4da3ab9ceef0
qreenify <48547831+qreenify@users.noreply.github.com>
2021-10-15 11:38:26 +0200

    project is set up

---
+++ Light/Assets/todo.cs
@@ -0,0 +1,5 @@
+﻿
+        //TODO: Create a room for the player to walk around in
+        //TODO: Create 2 players
+        //TODO: Add sprites to the scene
+        //TODO: Create the lightsource (Where there is light there should be color, less light = less colors)
